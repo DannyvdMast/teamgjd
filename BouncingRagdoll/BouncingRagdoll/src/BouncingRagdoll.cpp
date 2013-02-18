f968d1a72393eae3637153b3edc3845d0ca237a7
Danny van der Mast <danny@vdmastnet.nl>
2013-02-18 12:57:27 +0100

    Design upgrade

---
+++ BouncingRagdoll/BouncingRagdoll/src/BouncingRagdoll.cpp
@@ -0,0 +1,13 @@
+// BouncingRagdoll.cpp : Defines the entry point for the console application.
+//
+
+#include "..\Header\stdafx.h"
+#include <cyclone/cyclone.h> 
+#include <gl/glut.h>
+#include "..\Common Files\app.h"
+#include "..\Common Files\timing.h"
+
+class BouncingRagdoll : public RigidBodyApplication
+{
+
+}
